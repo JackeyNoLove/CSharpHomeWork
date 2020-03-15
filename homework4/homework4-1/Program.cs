27fb5c1ca2536212a8665eef9113bd851a0b770a
JackeyNoLove <a794388384@gmail.com>
2020-03-15 17:25:22 +0800

    2020/03/15update

---
+++ homework4/homework4-1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace homework4_1
+{
+    class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+        public class List<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public List()
+            {
+                tail = head = null;
+            }
+            public Node<T> Head
+            {
+                get => head;
+            }
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void ForEach(Action<T> action)
+            {
+                for (Node<T> node = head; node != null; node = node.Next)
+                {
+                    action(node.Data);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                list.Add(x);
+            }
+            list.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            int sum = 0, min = 0x7fffffff, max = 0;
+            list.ForEach(x => sum += x);
+            list.ForEach(x => { if (min > x) min = x; });
+            list.ForEach(x => { if (max < x) max = x; });
+            Console.WriteLine(sum);
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+        }
+    }
+}
---
+++ homework4/homework4-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace homework4_2
+{
+    public delegate void TickHandler(int CurrentTime);
+    public delegate void AlarmHandler();
+    public class Alarm
+    {
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+        public int CurrentTime=1;
+        public Alarm()
+        {
+            OnTick += tick;
+            OnAlarm += alarming;
+        }
+        public void Start()
+        {
+            for(CurrentTime = 1; CurrentTime < 100; CurrentTime++)
+            {
+                OnTick(CurrentTime);
+                Thread.Sleep(1000);
+            }
+        }
+        void tick(int CurrentTime)
+        {
+            if (CurrentTime % 10 == 0) OnAlarm();
+            else Console.WriteLine("现在时间:"+CurrentTime);
+        }
+        void alarming()
+        {
+            Console.WriteLine("整点报时！");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alarm alarm = new Alarm();
+            alarm.Start();
+        }
+    }
+}
